f360070939814a913dfb381153ceb0a24f774ed7
TheOMUU <omkarravindrarathod@gmail.com>
2026-09-16 19:50:07 +0530

    Added User entity and User repository in the Ecommerce backend

---
+++ src/main/java/com/example/ecommerce/entity/User.java
@@ -0,0 +1,48 @@
+package com.example.ecommerce.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import org.hibernate.annotations.AnyDiscriminatorImplicitValues;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private int age;
+
+    public User(){
+
+    }
+    public User(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
